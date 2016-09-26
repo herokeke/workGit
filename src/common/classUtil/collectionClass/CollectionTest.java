2fde3064a858f495d4fb56863ec146228d9a74ec
herokeke <2295054472@qq.com>
2016-09-26 10:46:11 +0800

    296

--- src/common/classUtil/collectionClass/CollectionTest.java
+++ src/common/classUtil/collectionClass/CollectionTest.java
@@ -1,8 +1,17 @@
 package common.classUtil.collectionClass;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CollectionTest {
 	 // Vector
     //Collections 框架
     //Java8中终于支持闭包了
     //java.util.concurrent 并发编程以及并发数据结构的设计
+	
+	Map map =new  HashMap();
+	
+	List list = new ArrayList();
 }
