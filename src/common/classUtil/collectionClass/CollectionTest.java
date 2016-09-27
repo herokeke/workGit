d89ebfa6072939ca9247f43bf6282e3a2ceee556
herokeke <2295054472@qq.com>
2016-09-27 09:37:07 +0800

    927

--- src/common/classUtil/collectionClass/CollectionTest.java
+++ src/common/classUtil/collectionClass/CollectionTest.java
@@ -2,8 +2,10 @@ package common.classUtil.collectionClass;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class CollectionTest {
 	 // Vector
@@ -14,4 +16,6 @@ public class CollectionTest {
 	Map map =new  HashMap();
 	
 	List list = new ArrayList();
+	
+	Set set = new HashSet();
 }
