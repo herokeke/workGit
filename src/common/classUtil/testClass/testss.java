3cc023e16d75325482e1d7251d710bfe696f7187
herokeke <2295054472@qq.com>
2016-10-13 15:01:30 +0800

    1013

--- src/common/classUtil/testClass/testss.java
+++ src/common/classUtil/testClass/testss.java
@@ -1,5 +1,25 @@
 package common.classUtil.testClass;
 
-public class testss {
+public class Testss implements Runnable {
+	
+	@Override
+	public void run() {
+	  System.out.println("执行");
+	}
 
+	public static void main(String[] args){
+		Runnable  test = new Testss();
+		Thread t1 = new Thread(test);
+		Thread t2 = new Thread(test);
+		//t.setName("dd");
+		
+		t2.start();
+		t1.start();
+		System.out.println(t1.getName());
+		System.out.println(t2.getName());
+		System.out.println(Thread.currentThread().getName());
+		
+	}
+
+	
 }
