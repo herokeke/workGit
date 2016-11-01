fa1696fd29bbccf68c25c9215aedf2adecb575e2
herokeke <2295054472@qq.com>
2016-11-01 09:26:09 +0800

    1101

--- src/common/classUtil/testClass/testss.java
+++ src/common/classUtil/testClass/testss.java
@@ -1,25 +1,57 @@
 package common.classUtil.testClass;
 
-public class testss implements Runnable {
-	
-	@Override
-	public void run() {
-	  System.out.println("执行");
-	}
 
+public class Testss {
+	
+	private static ThreadLocal actionContext = new ThreadLocal();
+	
+	public Testss(){
+		System.out.println("testss");
+	}
+	private static MyTest mt = new MyTest();
+
+	public static Testss gettestss(){
+		Testss  tt = (Testss) actionContext.get();
+		System.out.println("tt="+tt);
+		return tt;
+	}
+	public static void fun(){
+		System.out.println("why??");
+		mt.fun();
+	}
+	public static void excute(){
+		System.out.println("excute");
+	}
 	public static void main(String[] args){
-		Runnable  test = new testss();
-		Thread t1 = new Thread(test);
-		Thread t2 = new Thread(test);
-		//t.setName("dd");
-		
-		t2.start();
-		t1.start();
-		System.out.println(t1.getName());
-		System.out.println(t2.getName());
-		System.out.println(Thread.currentThread().getName());
-		
+		//System.out.println(Testss.class.hashCode());
+		Object o = Testss.gettestss();
+		int i=0;
+		while(true){
+			//System.out.println(Testss.gettestss().toString());
+			System.out.println("000>>"+Thread.currentThread().getName());
+			System.out.println("000>>"+Testss.gettestss());
+			Testss.gettestss().excute();
+			//Testss.fun();
+			if(i==10){
+				return; 
+			}
+			i++;
+		}
+	
 	}
+	
+	 protected void finalize(){
+		 System.out.println("gc");
+	 }
+	
 
 	
+}
+class MyTest{
+	public MyTest(){
+		System.out.println("mytest");
+	}
+	public void fun(){
+		System.out.println("fun");
+	}
 }
